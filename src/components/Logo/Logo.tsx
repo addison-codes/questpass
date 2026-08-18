00c87a2ec8e289674731d7b5d3211291dbd7b525
addison-codes <hi@addison.codes>
2026-08-18 11:29:34 -0400

    fix: colors

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -22,7 +22,7 @@ export const Logo = (props: Props) => {
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
+      className={clsx('w-full h-[134px]', className)}
       src="QuestPassLogo.png"
     />
   )
